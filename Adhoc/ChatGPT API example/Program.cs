3086ef137d6a44c132b25db907a3873a5920b676
iamshrijan <iamshrijan@gmail.com>
2023-01-26 23:36:24 +0530

    Adding Basic CHatGPT in CLI for the first parse

---
+++ Adhoc/ChatGPT API example/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+if (args.Length > 0)
+{
+    /* Improvement - Learn about Http Factory (For a bigger application)*/
+    HttpClient client = new HttpClient();
+
+    client.DefaultRequestHeaders.Add("authorization","Bearer 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"); //Never Paste Key to the Code (User Secrets) 
+
+    /* Create object and Serialize the request 
+    Learn what are the differences between the models*/
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \""+ args[0] +"\",\"temperature\": 1,\"max_tokens\": 100}",
+        Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+    
+    string responseString = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseString);
+    try 
+    {
+        var dyData = JsonConvert.DeserializeObject<dynamic>(responseString); /*Instead of using dynamic type object Create a object and desseialize that using that object*/
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"---> ChatGpt Response : {dyData!.choices[0].text}");
+        Console.ResetColor();
+
+    }
+    catch(Exception e)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(e.Message);
+        Console.ResetColor();
+    }
+}
+else 
+{
+    Console.WriteLine("Please provide at least one input");
+}
---
+++ Adhoc/ChatGPT API example/Readme.md
@@ -0,0 +1,22 @@
+# Readme.md
+
+### How to build the application 
+
+1. Epen cmd prompt in the project location 
+1. Execute the following commands in cmd 
+1. * ``` dotnet build ``` 
+1. * ``` dotnet publish -r win-x64 ```
+1. Add the ChatGPT.exe present in bin/in Environmental variables 
+
+### Improvements 
+
+1. Improvement - Learn about Http Factory (For a bigger application)
+1. Create object and Serialize the request instead of passing the Raw String
+1. Create and copy the google link with the same question in a clipboard
+
+### Need to Explore 
+
+1. Explore Documentation 
+1. Have a proper installer in the release with the fithub repo protected
+
+
